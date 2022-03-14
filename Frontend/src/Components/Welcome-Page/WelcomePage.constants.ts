7e44aff8208dca2b5742028b8bf79d73d83f09cc
Adam Birnbaum <adambiren19@gmail.com>
2022-03-14 16:36:54 +0200

    Added basic carousel design

---
+++ Frontend/src/Components/Welcome-Page/WelcomePage.constants.ts
@@ -0,0 +1,9 @@
+import bruceImage from "../../img/image1.jpg"
+import spiderImage from "../../img/image2.png"
+import avengersImage from "../../img/image3.jpg"
+
+export const CardsMockData = [
+    bruceImage,
+    spiderImage,
+    avengersImage,
+]
--- Frontend/src/Components/Welcome-Page/WelcomePage.tsx
+++ Frontend/src/Components/Welcome-Page/WelcomePage.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
+import { CardsMockData } from "./WelcomePage.constants";
 import './WelcomePage.css'
 
 function WelcomePage() {
+    
+
+function ValidImage() {
+    
+}
+
     return (
         <div className="WelcomePage__PageBody">
-            <div className="WelcomePage__ImageCarousel"></div>
-            <div className="WelcomePage__NameAndRating">
-                <p className="WelcomePage__MovieName">Bruce Almighty</p>
-                <ul className="WelcomePage__MovieRating">
-                    <li>&#9733;</li>
-                    <li>&#9733;</li>
-                    <li>&#9733;</li>
-                    <li>&#9733;</li>
-                    <li>&#9733;</li>
-                </ul>
+            <div className="WelcomePage__ImageCarousel">
+                {CardsMockData.map((src: string) => {
+                    return (
+                        <div className="WelcomePage__ImageCarousel__Container">
+                            <img  src={src} alt="is not displayed" className="WelcomePage__ImageCarousel__Image" />
+                            <div className="WelcomePage__NameAndRating">
+                                <p className="WelcomePage__MovieName">Movie Name</p>
+                                <ul className="WelcomePage__MovieRating">
+                                    <li>&#9733;</li>
+                                    <li>&#9733;</li>
+                                    <li>&#9733;</li>
+                                    <li>&#9733;</li>
+                                    <li>&#9733;</li>
+                                </ul>
+                            </div>
+                        </div>
+                    )
+                })}
+
+
+
+
             </div>
         </div>
     )
