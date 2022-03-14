ca20adfac7c9b7361bb4d6a692a95b91d91f1495
Eli <elikampel@yahoo.com>
2022-03-14 15:47:16 +0200

    toam fixed navbar

--- Frontend/src/Screens/GlobalComponents/NavBar/NavBar.tsx
+++ Frontend/src/Screens/GlobalComponents/NavBar/NavBar.tsx
@@ -3,18 +3,31 @@ import './NavBar.css';
 
 function Navbar() {
     return (
-        <div>
-            <div className='navbar'>
-                <h2 className='main_item'>FrameUp</h2>
-                <div className='sub_items'>
-                    <h3 className='item'><span>Popular</span></h3>
-                    <h3 className='item'><span>Movies</span></h3>
-                    <h3 className='item'><span>Series</span></h3>
-                    <h3 className='item'><span>Kids</span></h3>
-                </div>
+        <nav className='navbar-container'>
+            <div className="navbar-wrapper">
+                <h2 className="navbar-logo">FramUp</h2>
+                <ul className="navbar-sub-items">
+                    <li className="navbar-sub-item">Popular</li>
+                    <li className="navbar-sub-item">Movies</li>
+                    <li className="navbar-sub-item">Series</li>
+                    <li className="navbar-sub-item">Kids</li>
+                </ul>
             </div>
-        </div >
-    );
-};
 
-export default Navbar;
+        </nav>
+        );
+    };
+    
+    export default Navbar;
+    
+    // <div>
+    //     <div className='navbar'>
+    //         <h2 className='main_item'>FrameUp</h2>
+    //         <div className='sub_items'>
+    //             <h3 className='item'><span>Popular</span></h3>
+    //             <h3 className='item'><span>Movies</span></h3>
+    //             <h3 className='item'><span>Series</span></h3>
+    //             <h3 className='item'><span>Kids</span></h3>
+    //         </div>
+    //     </div>
+    // </div >
