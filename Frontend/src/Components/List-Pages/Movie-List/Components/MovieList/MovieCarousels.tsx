567b0a91ee5f756d007928e9da4fe14751cf7a3b
Adam Birnbaum <adambiren19@gmail.com>
2022-03-20 17:27:22 +0200

    List page mockup design

--- Frontend/src/Components/List-Pages/Movie-List/Components/MovieList/MovieCarousels.tsx
+++ Frontend/src/Components/List-Pages/Movie-List/Components/MovieList/MovieCarousels.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import './MovieCarouels.css';
+import './MovieCarousels.css';
 
 function MovieCarousels() {
     return(
-        <div className='MovieCarousels'>
-            <div className='MovieCarousels__Carousel1'></div>
+        <div className='MovieCarousels__Container'>
+            <div className='MovieCarousels__Carousel1'>
+                <div className="MovieCarousels__Carousel1__ButtonLeft">
+                    <p>&#9664;</p>
+                </div>
+                <div className="MovieCarousels__Carousel1__Card"></div>
+                <div className="MovieCarousels__Carousel1__Card"></div>
+                <div className="MovieCarousels__Carousel1__Card"></div>
+                <div className="MovieCarousels__Carousel1__Card"></div>
+                <div className="MovieCarousels__Carousel1__Card"></div>
+                <div className="MovieCarousels__Carousel1__ButtonRight">
+                    <p>&#9654;</p>
+                </div>
+            </div>
         </div>
     )
 }
+
+export default MovieCarousels
