f484e12598f9ccc7654f1e70c96a0901b214e696
Adam Birnbaum <adambiren19@gmail.com>
2022-03-21 16:49:19 +0200

    Added summary page component

---
+++ Frontend/src/Components/SummaryPage/SummaryPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './SummaryPage.css'
+
+function SummaryPage() {
+    return(
+        <div className="SummaryPage_Container">
+            <div className="SummaryPage">
+                Summary Page
+            </div>
+        </div>
+    )
+}
+
+export default SummaryPage;
