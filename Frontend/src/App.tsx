0e064c65de86b8ef9e3756b5add2df5d942eb05b
Adam Birnbaum <adambiren19@gmail.com>
2022-03-21 11:31:40 +0200

    Added Navbar to Home page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import WelcomePage from './Components/WelcomePage/WelcomePage';
 import NavBar from './Screens/GlobalComponents/NavBar/NavBar';
 
 function App() {
   return (
+    <>
       <NavBar from={{
         pathname: ''
-      }} prevState={null} location={undefined}/>
+      }} prevState={null} location={undefined} />
+      <WelcomePage />
+    </>
   );
 }
 
