7eb0008da251b1e65d092e89c3f7b96415fe3f47
Adam Birnbaum <adambiren19@gmail.com>
2022-03-22 21:29:26 +0200

    Added basic summary page layout and design

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import WelcomePage from './Components/Welcome-Page/WelcomePage';
 import Movies from './Components/List-Pages/Movie-List/Movies';
 import NavBar from './Screens/GlobalComponents/NavBar/NavBar';
+import SummaryPage from "./Components/SummaryPage/SummaryPage"
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <NavBar from={{
           pathname: ''
         }} prevState={null} location={undefined} />
-        <Movies />
+        <SummaryPage />
       </div>
     </>
   )
--- Frontend/src/Components/SummaryPage/SummaryPage.tsx
+++ Frontend/src/Components/SummaryPage/SummaryPage.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import './SummaryPage.css'
 
 function SummaryPage() {
-    return(
+    return (
         <div className="SummaryPage_Container">
-            <div className="SummaryPage">
-                Summary Page
+            <div className="SummaryPage__Image"></div>
+            <div className="SummaryPage__TextArea">
+                <h2 className="SummaryPage__Text__Header">Lorem ipsum dolor</h2>
+                <p className="SummaryPage__Text__Paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque ultrices neque, ut fringilla turpis volutpat eget. Proin semper consequat sapien, ac vehicula sem maximus congue. Nullam sagittis bibendum scelerisque. Donec velit sapien, hendrerit in augue at, lobortis congue augue. Nam vulputate tellus at Vestibulum ultrices sit amet quam vel rutrum.
+                <br /><br />
+                Morbi sed mauris quis purus commodo fermentum. Integer sed feugiat purus, eu sagittis libero. Cras a magna non neque aliquam fringilla et et tortor. Quisque auctor elit leo, quis egestas enim efficitur vel. Mauris elementum enim lectus, ut laoreet velit mattis sit amet. Proin sit amet mollis lectus. Pellentesque id orci purus. Nam semper ornare ante, non sagittis erat. Vestibulum ut tempus ligula. Nam fringilla nibh cursus nulla varius tempus.</p>
             </div>
         </div>
     )
